8720cf6264eabcd03577485e065d1935031c6c2c
IZeike <harit20451@outlook.com>
2016-10-06 23:11:04 +0700

    add photo in assets

---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,27 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by harit on 10/6/2016.
+ */
+public class GameScreen extends ScreenAdapter {
+    private PacmanGame pacmanGame;
+    private Texture pacmanImg;
+
+    public GameScreen(PacmanGame pacmanGame)
+    {
+        this.pacmanGame = pacmanGame;
+        pacmanImg = new Texture("pacman.png");
+    }
+
+    public  void render (float delta)
+    {
+        SpriteBatch batch = pacmanGame.batch;
+        batch.begin();
+        batch.draw(pacmanImg,100,100);
+        batch.end();
+    }
+}
